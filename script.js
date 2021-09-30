001c1f15321bdf7e2409d8081f0720191b5a763d
Ilker Karakurt <i_love_history96@mail.ru>
2021-10-01 00:05:35 +0500

    Выполнил первое домашнее задание

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "JS 21.0 | ПОЛНЫЙ ФАРШ";
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+
+console.log("Введение в язык, подключение javascript. Основы работы с Git.");
+alert(title);
