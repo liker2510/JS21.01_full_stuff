dc744835bd321145d6aedd0d519029aefc53dab4
Ilker Karakurt <i_love_history96@mail.ru>
2021-10-01 21:43:17 +0500

    Выполнил второе домашнее задание

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const title = "JS 21.0 | ПОЛНЫЙ ФАРШ";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = "project_js21";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let rollback = 5;
+let fullPrice = 5600000;
+let adaptive = true;
+console.log(typeof title, fullPrice, adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} долларов и Стоимость разработки сайта ${fullPrice} долларов`);
+screens = screens.toLowerCase();
 
 
-console.log("Введение в язык, подключение javascript. Основы работы с Git.");
-alert(title);
+console.log(screens.split(", "));
+console.log (fullPrice * (rollback/100));
