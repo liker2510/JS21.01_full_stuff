e54374f479a4622ff4248d7fbabb539b19a986b3
Ilker Karakurt <i_love_history96@mail.ru>
2021-10-10 20:16:44 +0500

    Выполнил усложненое задание четвертого урока

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-let num = 266219;
-let splitNum = num.toString().split('');
-let convertNum = splitNum.map(Number);
-console.log(convertNum);
+'use strict';
 
-let result = convertNum.reduce((acc, rec) => acc * rec);
-console.log(result);
+let argument1 = 25;
+let argument2 = "Строока";
+let argument3 = "    Строока    ";
+let argument4 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aperiam repudiandae harum, explicabo maxime ipsum asperiores exercitationem! Sint ad pariatur nihil eveniet repudiandae facere tempora nesciunt explicabo fugiat, error exercitationem!"
 
-let degree = result ** 3;
-console.log(String(degree).slice(0,2));
-
+function getArgument(argument) {
+    
+    if (typeof argument !== 'string') {
+        console.log('Введена не строка');
+        return;
+    }
+    argument = argument.trim();
+    if (argument.length <= 30) return argument;
+    argument = argument.slice(0, 30) + '...';
+    return argument;
+  }
+  console.log(getArgument(argument1));
+  console.log(getArgument(argument2));
+  console.log(getArgument(argument3));
+  console.log(getArgument(argument4));
